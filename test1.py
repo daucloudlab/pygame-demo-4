10e00a720b97129008784198573dcec13ca549ec
Daulet <daucloudlab@gmail.com>
2021-11-14 19:24:02 +0600

    first window demo

--- test1.py
+++ test1.py
@@ -5,3 +5,15 @@ DISPLAY_HEIGHT = 400
 
 pygame.init()
 display_surface = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+pygame.display.set_caption("Hello!")
+
+running = True
+while running:
+    
+    for event in pygame.event.get():
+        print(event)
+
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
