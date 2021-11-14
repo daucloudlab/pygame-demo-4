b8ff39a1a9d21507fe305dda7b0777ce07767b63
Daulet <daucloudlab@gmail.com>
2021-11-14 19:50:46 +0600

    draw various shapes

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 import pygame
 
-DISPLAY_WIDTH = 500
-DISPLAY_HEIGHT = 400
+DISPLAY_WIDTH = 600
+DISPLAY_HEIGHT = 600
 
 pygame.init()
 display_surface = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
@@ -17,6 +17,15 @@ CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 
 display_surface.fill(BLUE)
+
+pygame.draw.line(display_surface, RED, (0,0), (100, 100), 5)
+pygame.draw.line(display_surface, GREEN, (100, 100), (200, 300), 1)
+
+pygame.draw.circle(display_surface, WHITE, (DISPLAY_WIDTH//2, DISPLAY_HEIGHT//2), 200, 6)
+pygame.draw.circle(display_surface, YELLOW, (DISPLAY_WIDTH//2, DISPLAY_HEIGHT//2), 100, 0)
+
+pygame.draw.rect(display_surface, CYAN, (500, 0, 100, 100))
+pygame.draw.rect(display_surface, MAGENTA, (500, 100, 50, 100), 6)
 
 running = True
 while running:
