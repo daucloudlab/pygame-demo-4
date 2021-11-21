76839d4e604a69e8b458e74a0187270b004a994c
Daulet <daucloudlab@gmail.com>
2021-11-21 15:47:08 +0600

    added new shapes

--- draw_shapes.py
+++ draw_shapes.py
@@ -19,10 +19,16 @@ pygame.display.set_caption("Draw Shapes")
 display_surface.fill(BLUE)
 
 pygame.draw.line(display_surface, WHITE, (0, 0), (300, 200), 4)
+pygame.draw.aaline(display_surface, WHITE, (50, 50), (300, 250))
+pygame.draw.lines(display_surface, WHITE, True, [(100, 100), (150, 150), (125, 200)], 5)
+pygame.draw.aalines(display_surface, WHITE, True, [(200, 200), (250, 250), (225, 400)])
+pygame.draw.polygon(display_surface, WHITE, [(100, 150),(150, 200), (150, 300)])
+pygame.draw.rect(display_surface, CYAN, (WINDOW_WIDTH//2, 0, 200, 200))
 pygame.draw.circle(display_surface, RED, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2), 100, 4)
 pygame.draw.circle(display_surface, YELLOW, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2), 50, 0)
-pygame.draw.rect(display_surface, CYAN, (WINDOW_WIDTH//2, 0, 200, 200))
-
+pygame.draw.ellipse(display_surface, WHITE, (200, 400, 250, 150), 1)
+pi=3.1415
+pygame.draw.arc(display_surface, WHITE, (100, 500, 200, 100), pi/2, 0, 4)
 running = True
 while running:
     for event in pygame.event.get():
